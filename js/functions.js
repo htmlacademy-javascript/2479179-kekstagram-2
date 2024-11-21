923ba9da9a037d59a70b8075bd597626e70e17ae
Alina Valenda <alinavalendo1995@gmail.com>
2024-11-21 19:16:29 +0100

    Тренировочные функции.

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+const checkLength = (string = '', maxSymbols = 1) => string.length <= maxSymbols;
+
+function comparesStringLength(string, length) {
+  if (string.length <= length) {
+    return true;
+  }
+}
+
+//  ******************
+function isPalindrome(string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return string === reversed;
+}
+
+//  ******************
+const extractNumbers = (string) => {
+  let result = '';
+  string = string.toString();
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (Number.isNaN (parseInt (string[i], 10)) === false) {
+      result += string[i];
+    }
+  }
+
+  return result === '' ? NaN : Number(result);
+};
