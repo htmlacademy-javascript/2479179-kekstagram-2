7e7c8b3f13addc43ab6d8905d96b0d507fde13be
Alina Valenda <alinavalendo1995@gmail.com>
2025-01-06 17:25:12 +0100

    Модуляция

--- js/functions.js
+++ js/functions.js
@@ -22,7 +22,7 @@ const extractNumbers = (string) => {
   let result = '';
   string = string.toString();
   for (let i = 0; i <= string.length - 1; i++) {
-    if (Number.isNaN (parseInt (string[i], 10)) === false) {
+    if (Number.isNaN(parseInt(string[i], 10)) === false) {
       result += string[i];
     }
   }
