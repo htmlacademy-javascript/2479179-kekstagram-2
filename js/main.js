ffe2e7a2eba65459c40e17295a83010bd1fab75d
Alina Valenda <alinavalendo1995@gmail.com>
2025-01-24 20:42:50 +0100

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ import { generatePhotos } from './photos';
 // Генерация массива фотографий
 const generatedPhotos = generatePhotos();
 console.log(generatedPhotos);
+
+// Отрисовываем миниатюры
+import { renderThumbnails } from './thumbnails.js';
+renderThumbnails();
---
+++ js/thumbnails.js
@@ -0,0 +1,39 @@
+// Импортируем данные о фотографиях
+import { generatePhotos } from './photos.js';
+
+// Функция для отрисовки миниатюр
+export function renderThumbnails() {
+  // Генерируем данные о фотографиях
+  const photos = generatePhotos();
+
+  // Находим контейнер, куда будем добавлять миниатюры
+  const picturesContainer = document.querySelector('.pictures');
+
+  // Находим шаблон миниатюры
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+  // Создаём DocumentFragment для вставки элементов
+  const fragment = document.createDocumentFragment();
+
+  // Обходим каждую фотографию из сгенерированных данных
+  photos.forEach((photo) => {
+    // Клонируем шаблон элемента
+    const pictureElement = pictureTemplate.cloneNode(true);
+
+    // Заполняем шаблон данными фотографии
+    const pictureImg = pictureElement.querySelector('.picture__img'); // Изображение
+    const pictureLikes = pictureElement.querySelector('.picture__likes'); // Лайки
+    const pictureComments = pictureElement.querySelector('.picture__comments'); // Комментарии
+
+    pictureImg.src = photo.url; // Устанавливаем путь к изображению
+    pictureImg.alt = photo.description; // Описание для alt
+    pictureLikes.textContent = photo.likes; // Количество лайков
+    pictureComments.textContent = photo.comments.length; // Количество комментариев
+
+    // Добавляем миниатюру в DocumentFragment
+    fragment.appendChild(pictureElement);
+  });
+
+  // Вставляем все миниатюры в контейнер .pictures
+  picturesContainer.appendChild(fragment);
+}
