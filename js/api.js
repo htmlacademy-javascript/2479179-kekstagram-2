64d441acc4c2134fb8a9b6b4d04859b4e7240511
Alina Valenda <alinavalendo1995@gmail.com>
2025-03-11 17:37:35 +0100

    финал 1

--- js/api.js
+++ js/api.js
@@ -1,61 +1,34 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
 const SERVER_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
 
-
-/** Массив с сервера
-@returns {Promise<Array>} */
-
-async function fetchData() {
-  try {
-    const response = await fetch(`${SERVER_URL}/data`);
-    if (!response.ok) {
-      throw new Error(`Ошибка загрузки: ${response.status}`);
-    }
-    const data = await response.json();
-    if (!data || !Array.isArray(data)) {
-      throw new Error('Некорректные данные с сервера');
-    }
-
-    return data;
-  } catch (error) {
-    console.error('Ошибка получения данных:', error);
-    alert('Ошибка загрузки данных. Проверьте интернет-соединение.');
-    return []; // Возврат пустого массива
-  }
-}
-
-
-/**
- * Отправка данных формы и изображения.
- * @param {HTMLFormElement} formElement - Форма
- * @returns {Promise<Object|null>} - Ответ
- */
-
-const sendFormData = async (formElement) => {
-  const formData = new FormData(formElement);
-  const submitButton = formElement.querySelector('.img-upload__submit');
-
-  submitButton.disabled = true; // Блокировка кнопки
-
-  try {
-    const response = await fetch(SERVER_URL, {
-      method: 'POST',
-      body: formData, // Отпрака данных multipart/form-data
-    });
-
-    if (!response.ok) {
-      throw new Error(`Ошибка отправки формы: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error('Ошибка отправки формы:', error);
-    return null;
-  } finally {
-    submitButton.disabled = false; // Анлок кнопки
-  }
+const serverRoute = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
 };
 
-export { fetchData, sendFormData };
+const httpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+};
 
+const errorText = {
+  [httpMethod.GET]: 'Failed to load data',
+  [httpMethod.POST]: 'Failed to send data',
+};
+
+async function request(url, method = httpMethod.GET, body = null) {
+  const response = await fetch(url, { method, body });
+  if (!response.ok) {
+    throw new Error(errorText[method]); // Ошибка запроса
+  }
+  return response.json(); // Преобразование данных
+}
+
+async function loadPictures() {
+  return request(SERVER_URL + serverRoute.GET_DATA); // Получить данные
+}
+
+async function sendPictures(pictureData) {
+  return request(SERVER_URL + serverRoute.SEND_DATA, httpMethod.POST, pictureData); // Отправить данные
+}
+
+export { loadPictures, sendPictures };
